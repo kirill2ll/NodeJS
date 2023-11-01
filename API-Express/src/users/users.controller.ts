44f7a45743af762275cbaa79a891989b5981bec2
kirill2ll <kirill2ll@mail.ru>
2023-11-01 21:35:49 +0200

    Add Users Controller Interface

--- API-Express/src/users/users.controller.ts
+++ API-Express/src/users/users.controller.ts
@@ -2,8 +2,9 @@ import { BaseController } from "../common/base.controller";
 import { HTTPError } from "../errors/http-error.class";
 import { LoggerService } from "../logger/logger.service";
 import { Request, Response, NextFunction } from "express";
+import { IUserController } from "./users.controller.interface";
 
-export class UserController extends BaseController {
+export class UserController extends BaseController implements IUserController {
   constructor(logger: LoggerService) {
     super(logger);
     this.bindRoutes([
